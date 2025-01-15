6fc016d976342041a019d2476198744157966911
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-01-15 19:05:01 +0530

    Spring Framework core

---
+++ src/main/java/lk/ijse/bean/MyConnection.java
@@ -0,0 +1,13 @@
+package lk.ijse.bean;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+
+public class MyConnection {
+
+    public  MyConnection() {
+        System.out.println("MyConnection Object created");
+    }
+}
